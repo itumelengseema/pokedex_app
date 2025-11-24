d6455b435250a5d36ebaa60fc39e3dac2f30fa96
Itumeleng Seema <itumelengseema@outlook.com>
2025-11-24 10:38:38 +0200

    Implement bottom navigation bar in MainApp; refactor MainApp to StatefulWidget for navigation state management

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,15 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +21,24 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(child: Center(child: HomeScreen())),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            // Handle navigation bar item selection
+            setState(() {
+              currentPage = index;
+            });
+          },
+          indicatorColor: Colors.red[400]!,
+          selectedIndex: currentPage,
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ),
       ),
     );
   }
